bc4642c2f3f50fc4f40cdb9b3b7c66654f99eabe
ewerthonjust <ewerthonjust@gmail.com>
2017-09-12 19:41:29 -0300

    12/09

--- backend/src/modelo/Produto.java
+++ backend/src/modelo/Produto.java
@@ -1,5 +1,61 @@
 package modelo;
 
+import java.sql.Date;
+
 public class Produto {
 
+	private int id;
+	private int idProduto;
+	private Double quantidade;
+	private Double valor;
+	private String descricao;
+	
+	public Produto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Produto(int idProduto, Double quantidade, Double valor, String descricao) {
+		super();
+		this.idProduto = idProduto;
+		this.quantidade = quantidade;
+		this.valor = valor;
+		this.descricao = descricao;
+	}
+
+	public int getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public Double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Double quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
 }
--- backend/src/modelo/Venda.java
+++ backend/src/modelo/Venda.java
@@ -1,5 +1,72 @@
 package modelo;
 
+import java.sql.Date;
+
 public class Venda {
 
-}
+	
+	private int id;
+	private int idProduto;
+	private Double quantidade;
+	private Double valor;
+	private Date data;
+	private boolean statusPagamento;
+	
+	public Venda() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Venda(int idProduto, Double quantidade, Double valor, Date data, boolean statusPagamento) {
+		super();
+		this.idProduto = idProduto;
+		this.quantidade = quantidade;
+		this.valor = valor;
+		this.data = data;
+		this.statusPagamento = statusPagamento;
+	}
+
+	public int getNomeProduto() {
+		return idProduto;
+	}
+
+	public void setNomeProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public Double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Double quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public boolean isStatusPagamento() {
+		return statusPagamento;
+	}
+
+	public void setStatusPagamento(boolean statusPagamento) {
+		this.statusPagamento = statusPagamento;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	}
